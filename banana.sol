cde96b329cee79bf4eac5b79e2041ff4d4cc04d8
tgalantini <92930673+tgalantini@users.noreply.github.com>
2022-07-03 15:02:10 +0200

    Update banana.sol

--- banana.sol
+++ banana.sol
@@ -6,8 +6,9 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 pragma solidity ^0.8.6;
 
-abstract contract NFTees {
-    function ownerOf(uint256 tokenId) public view virtual returns (address owner);
+interface IERC721 is IERC165 {
+
+    function ownerOf(uint256 tokenId) external view returns (address owner);
 }
 
 contract Banana is ERC1155, Ownable {
@@ -17,21 +18,21 @@ contract Banana is ERC1155, Ownable {
     uint256 public bananaPrice = 0.05 ether;
     mapping (uint256 => uint256) private AlreadyClaimed;
     address SuperBanana;
-    address nftees = 0x8F3819b281012fBa163c57100d5d819798FE12f6;
-    NFTees private immutable SC;
+    IERC721 NFTEE = IERC721(0x8F3819b281012fBa163c57100d5d819798FE12f6);
     
     constructor() ERC1155("") {
-        SC = NFTees(nftees);
     }
 
     function mint(uint256 amount, uint256 NfTeesId) public payable {
         require(amount + _minted <= MAX_SUPPLY, "Banana: Exceed max supply");
-        require(SC.ownerOf(NfTeesId) == msg.sender, "Must be Owner of OG collection to mint");
+        address owner = NFTEE.ownerOf(NfTeesId);
+        require(owner == msg.sender, "Must be Owner of OG collection to mint");
         require(msg.value == amount * bananaPrice, "Invalid funds provided");
         require(AlreadyClaimed[NfTeesId] < 2 , "Token already claimed");
         _minted += amount;
-        AlreadyClaimed[NfTeesId] += AlreadyClaimed[NfTeesId];
+        AlreadyClaimed[NfTeesId] += amount;
         _mint(msg.sender, 0, amount, "");
+        delete owner;
     }
 
     function burnBanana(address burnTokenAddress) external {
