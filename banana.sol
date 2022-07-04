ab6133f7717831f494257b95ff5f15cfb5e9b47b
tgalantini <92930673+tgalantini@users.noreply.github.com>
2022-07-04 16:50:47 +0200

    Update banana.sol

--- banana.sol
+++ banana.sol
@@ -4,35 +4,59 @@ pragma solidity ^0.8.11;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-pragma solidity ^0.8.6;
-
 interface IERC721 is IERC165 {
 
     function ownerOf(uint256 tokenId) external view returns (address owner);
 }
 
+pragma solidity ^0.8.6;
+
+
 contract Banana is ERC1155, Ownable {
-    uint256 public constant MAX_SUPPLY = 8888;
+
+    // Publc Constants
+    uint256 public constant MAX_SUPPLY = 10044;
+    uint256 public constant MAX_MINTS = 2;
+    uint256 public constant BANANA_PRICE = 0.001 ether;
+    string public name;
+    string public symbol;
+
+    // Variables
     string private baseURI;
     uint256 public _minted = 0;
-    uint256 public bananaPrice = 0.05 ether;
-    mapping (uint256 => uint256) private AlreadyClaimed;
     address SuperBanana;
-    IERC721 NFTEE = IERC721(0x8F3819b281012fBa163c57100d5d819798FE12f6);
-    
-    constructor() ERC1155("") {
-    }
 
-    function mint(uint256 amount, uint256 NfTeesId) public payable {
+    // Sale controllers
+    bool public PublicPaused = true;
+    bool public WhitelistPaused = true;
+
+    // Mappings
+    mapping(address => uint256) public addressPublicMintedBalance;
+    mapping (uint256 => uint256) private AlreadyClaimed;
+
+    // Instance of God Hates Nftees smart contract
+    IERC721 NFTEE = IERC721(0xAc193170027F642aaE45d0F3CfcB11A42f150606);
+    
+    constructor(
+        string memory _uri,
+        string memory _name,
+        string memory _symbol
+    ) ERC1155(_uri) {
+        name = _name;
+        symbol = _symbol;
+    }
+  
+    function whitelistMint(uint256 amount, uint256 NfTeesId) public payable {
+        require(!WhitelistPaused, "Whitelist sale is paused");
         require(amount + _minted <= MAX_SUPPLY, "Banana: Exceed max supply");
-        address owner = NFTEE.ownerOf(NfTeesId);
-        require(owner == msg.sender, "Must be Owner of OG collection to mint");
-        require(msg.value == amount * bananaPrice, "Invalid funds provided");
+        address _owner = NFTEE.ownerOf(NfTeesId);
+        require(_owner == msg.sender, "Must be Owner of OG collection to mint");
+        require(msg.value == amount * BANANA_PRICE, "Invalid funds provided");
         require(AlreadyClaimed[NfTeesId] < 2 , "Token already claimed");
         _minted += amount;
         AlreadyClaimed[NfTeesId] += amount;
         _mint(msg.sender, 0, amount, "");
-        delete owner;
+        delete _owner;
     }
     
     function publicMint(uint256 amount) public payable {
@@ -40,7 +64,8 @@ contract Banana is ERC1155, Ownable {
         require(amount > 0 && amount <= MAX_MINTS);
         uint256 addressPublicMintedCount = addressPublicMintedBalance[msg.sender];
         require(addressPublicMintedCount + amount <= MAX_MINTS, "max NFT per address exceeded");
-        require(msg.value == amount * bananaPrice, "Invalid funds provided");
+        require(msg.value == amount * BANANA_PRICE, "Invalid funds provided");
+        addressPublicMintedBalance[msg.sender] += amount;
         _mint(msg.sender, 0, amount, "");
         delete addressPublicMintedCount;
     }
@@ -62,12 +87,13 @@ contract Banana is ERC1155, Ownable {
         SuperBanana = SuperBananaAddress;
     }
 
-    function updateBaseUri(string memory _baseURI) external onlyOwner {
-        baseURI = _baseURI;
+    function updateUri(string memory _newUri) external onlyOwner {
+        _setURI(_newUri);
     }
-    
+
     function withdrawMoney() external onlyOwner {
       (bool success, ) = msg.sender.call{value: address(this).balance}("");
       require(success, "WITHDRAW FAILED!");
     }
+
 }
